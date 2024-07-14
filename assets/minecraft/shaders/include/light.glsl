18d0acc3440006b065637a068a0ffea16c00cdf3
Blockybomb <118032108+BlockybombDEV@users.noreply.github.com>
2024-07-14 16:41:14 +0200

    pack wowo

--- assets/minecraft/shaders/include/light.glsl
+++ assets/minecraft/shaders/include/light.glsl
@@ -1,7 +1,13 @@
 #version 150
 
+#moj_import <light_config.glsl>
+
 #define MINECRAFT_LIGHT_POWER   (0.6)
 #define MINECRAFT_AMBIENT_LIGHT (0.4)
+
+float getPerFaceLighting(vec3 normalWS) {
+    return dot(abs(normalWS) * vec3(0.6, 0.75 + 0.25 * sign(normalWS.y), 0.8), vec3(1.0));
+}
 
 vec4 minecraft_mix_light(vec3 lightDir0, vec3 lightDir1, vec3 normal, vec4 color) {
     lightDir0 = normalize(lightDir0);
@@ -14,4 +20,11 @@ vec4 minecraft_mix_light(vec3 lightDir0, vec3 lightDir1, vec3 normal, vec4 color
 
 vec4 minecraft_sample_lightmap(sampler2D lightMap, ivec2 uv) {
     return texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0)));
+}
+
+vec4 minecraft_sample_lightmap_custom(sampler2D lightMap, ivec2 uv, float faceLighting) {
+    #ifdef CUSTOM_LIGHTING
+    faceLighting = mix(faceLighting, 1.0, uv.x / 256.0);
+    #endif
+    return texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0))) * faceLighting;
 }
